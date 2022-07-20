9d01c0ca1be63fdc69254b385df1b7214aa325d2
Adrian de Freitas <Adefreit@users.noreply.github.com>
2022-07-20 15:18:15 -0600

    Uploading Template

---
+++ a02.c
@@ -0,0 +1,18 @@
+/** a02-01.c
+* ===========================================================
+* Name: 
+* Section:
+* Project: Assessment 2 Main Program
+* ===========================================================
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "a02functs.h"
+
+int main() {
+
+    // YOUR CODE GOES HERE
+
+    return 0;
+}
---
+++ a02functs.h
@@ -0,0 +1,14 @@
+/** a02functs.h
+* ===========================================================
+* Name: 
+* Section:
+* Project: Assessment 2 Function Declarations
+* ===========================================================
+*/
+
+#ifndef A02_FUNCTS
+#define A02_FUNCTS
+
+// YOUR CODE GOES HERE
+
+#endif
